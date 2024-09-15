6174a1660836f11453c66b1bd18d8d70371654c6
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-09-14 22:07:28 -0500

    Game running successfully in terminal. Would like to build webpage for this.

--- src/main/java/com/coderyan/demo/NumberGuessingGameApplication.java
+++ src/main/java/com/coderyan/demo/NumberGuessingGameApplication.java
@@ -3,11 +3,56 @@ package com.coderyan.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class NumberGuessingGameApplication {
+	public static void guessingNumberGame(){
+		Scanner sc = new Scanner(System.in);
+
+		int number = 1 + (int)(100 * Math.random());
+
+		int K = 5;
+
+		int i, guess;
+
+		System.out.println(
+				"A number is chosen"
+				+ " between 1 to 100."
+				+ " Guess the number"
+				+" within 5 trials.");
+
+		for (i = 0; i < K; i++) {
+			System.out.println("Guess the number:");
+
+			guess = sc.nextInt();
+
+			if (number == guess) {
+				System.out.println(
+						"Congradulations!"
+						+ " You have guess the number."
+				);
+				break;
+			}
+			else if (number > guess && i != K -1) {
+				System.out.println("The number is "
+				+ "greater than " + guess);
+			}
+			else if (number < guess && i != K-1) {
+				System.out.println("The number is"
+				+ " less than " + guess);
+			}
+		}
+		if (i == K) {
+			System.out.println("You have exhausted " +  K + " trials");
+			System.out.println("The number was " + number);
+		}
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(NumberGuessingGameApplication.class, args);
+		guessingNumberGame();
+//		SpringApplication.run(NumberGuessingGameApplication.class, args);
 	}
+
 
 }
